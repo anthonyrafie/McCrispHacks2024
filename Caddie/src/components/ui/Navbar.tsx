43c1fc20f1294d9a13861c400f0eb7cc1f324dda
anthonyazrak <anthony.azrak1@gmail.com>
2024-01-28 09:47:27 -0500

    updates

--- Caddie/src/components/ui/Navbar.tsx
+++ Caddie/src/components/ui/Navbar.tsx
@@ -1,7 +1,132 @@
+import { IoIosCall } from "react-icons/io";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+
+import React from "react";
+import { Button } from "@/components/ui/button"
+import { FaHistory } from "react-icons/fa";
+import { Skeleton } from "@/components/ui/skeleton"
+
+  
 const Navbar = () => {
     return (
       <div className="flex justify-center items-center bg-black"> {/* Added flex, justify-center, and py-4 for padding */}
         <p className="text-white font-semibold text-[40px]">Caddie</p>
+        <Sheet>
+  <SheetTrigger>
+    <Button variant={"default"} className="absolute text-white font-bold rounded right-0 top-0 m-4">
+      <FaHistory size={20} className="inline-block"/>
+    </Button>
+  </SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Call History</SheetTitle>
+      <SheetDescription>
+        See prior call details at a glance.
+      </SheetDescription>
+    </SheetHeader>
+    <div className="overflow-auto max-h-[750px] mt-4">
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 my-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+    </div>
+  </SheetContent>
+</Sheet>
+
       </div>
     );
   };
